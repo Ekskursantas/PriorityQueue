61b722ff1c44ca742a37a761d39b700829d65307
Ekskursantas <pr.zygimantas@gmail.com>
2017-05-30 03:49:12 +0200

    End game

--- src/main/java/dk/cphbusiness/algorithm/examples/queues/PrioritisingPassenger.java
+++ src/main/java/dk/cphbusiness/algorithm/examples/queues/PrioritisingPassenger.java
@@ -11,42 +11,43 @@ import dk.cphbusiness.airport.template.Plane;
 import dk.cphbusiness.airport.template.Time;
 import java.util.Date;
 
-/**
- *
- * @author Daniel
- */
+
 public class PrioritisingPassenger implements PriorityQueue<Passenger> {
 
     private final Passenger[] items;
     private int size = 0;
 
     public static void main(String[] args) {
-        PrioritisingPassenger pp = new PrioritisingPassenger(100);
-        for (int i = 0; i < 30; i++) {
-            pp.enqueue(new Passenger(i, new Time(new Date().getTime()), pp.randomCat(), new Plane(new Time(new Date().getTime()))));
+        PrioritisingPassenger enlistedPeople = new PrioritisingPassenger(100);
+        for (int i = 0; i < 100; i++) {
+            enlistedPeople.enqueue(new Passenger
+                (i
+                , new Time(new Date().getTime())
+                , enlistedPeople.categorizer()
+                , new Plane(new Time(new Date().getTime()))));
         }
-        pp.printQueue();
-        for (int i = 0; i < 30; i++) {
-            System.out.println(pp.dequeue().getCategory());
+        
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + " " +enlistedPeople.dequeue().getCategory());
             System.out.println("");
         }
     }
 
-    private Category randomCat() {
+    private Category categorizer() {
         int rand = (int) (Math.random() * 5);
-        if (rand == 0) {
-            return Category.Monkey;
-        }
-        if (rand == 1) {
-            return Category.Disabled;
-        }
-        if (rand == 2) {
-            return Category.Family;
-        }
-        if (rand == 3) {
-            return Category.BusinessClass;
-        }
-        return Category.LateToFlight;
+        switch (rand){
+            case 0:
+                return Category.Monkey;
+            case 1:
+                return Category.Disabled;
+            case 2:
+                return Category.Family;
+            case 3:
+                return Category.BusinessClass;
+            default:
+                return Category.LateToFlight;
+                
+        }  
     }
 
     public PrioritisingPassenger(int capacity) {
@@ -55,10 +56,10 @@ public class PrioritisingPassenger implements PriorityQueue<Passenger> {
 
     @Override
     public void enqueue(Passenger item) {
-        //Passenger temp = null;
         items[++size] = item;
         int position = size;
-        while (items[parent(position)] != null && items[position].compareTo(items[parent(position)]) < 0) {
+        while (items[parent(position)] != null 
+                && items[position].compareTo(items[parent(position)]) < 0) {
             swap(position, parent(position));
             position = parent(position);
         }
@@ -67,28 +68,22 @@ public class PrioritisingPassenger implements PriorityQueue<Passenger> {
 
     @Override
     public Passenger dequeue() {
-        // Reference for item with highest priority
         Passenger first = items[1];
-        // Move last item on top
         items[1] = items[size];
         items[size] = null;
         size--;
         int pos = 1;
-        // Trickle down until childelements are either higher or null
-        // While both children are there...
         while (leftChild(pos)<= size  && rightChild(pos)< size ) {
-            //Find smallest child and swap
+            
             if (items[leftChild(pos)].compareTo(items[rightChild(pos)]) <= 0 
-                    && items[pos].compareTo(items[leftChild(pos)])>0){
-//                System.out.println("Left child: "+items[leftChild(pos)].getCategory() 
-//                        + "should be higher up than pos " +items[pos].getCategory());
+                    && items[pos].compareTo(items[leftChild(pos)])>0)
+            {
                 swap(pos,leftChild(pos));
                 pos = leftChild(pos);
             } 
             else if (items[leftChild(pos)].compareTo(items[rightChild(pos)]) > 0
-                    && items[pos].compareTo(items[rightChild(pos)])>0){
-//      System.out.println("Right child: "+items[rightChild(pos)].getCategory() 
-//                        + "should be higher up than pos " +items[pos].getCategory());
+                    && items[pos].compareTo(items[rightChild(pos)])>0)
+            {
                 swap(pos,rightChild(pos));
                 pos = rightChild(pos);
             }
@@ -97,8 +92,6 @@ public class PrioritisingPassenger implements PriorityQueue<Passenger> {
         }
         if (leftChild(pos)<= size && items[pos].compareTo(items[leftChild(pos)])>0) swap(pos,leftChild(pos));
     
-        printQueue();
-        //System.out.println("First in line is a: "+first.getCategory());
         return first;
     }
     private int parent(int position){
@@ -126,23 +119,5 @@ public class PrioritisingPassenger implements PriorityQueue<Passenger> {
         items[pos1] = items[pos2];
         items[pos2] = items[0];
     }
-void printQueue(){
-    if (true) return;
-    String line = "";
-    int numsOnLine = 1;
-    for (int i = 1; i < size; i++) {
-        if (i == numsOnLine){
-            line+= System.lineSeparator();
-            numsOnLine*=2;
-        }
-        for (int j = 0; j < size/(numsOnLine); j++) {
-            line += "  ";
-        }
-        line+= items[i].getCategory();
-        for (int j = 0; j < size/(numsOnLine); j++) {
-            line += "  ";
-        }
-    }
-    System.out.println(line);
-}
+
 }
